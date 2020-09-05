7e0135a634461c1f2add99b35079ec204544cfdc
Matthieu Mourisson <mareek@gmail.com>
2020-09-05 15:59:27 +0200

    Started Tile Tree implementation

---
+++ NetElevation.Core.Test/TileTreeNodeTest.cs
@@ -0,0 +1,39 @@
+﻿using NFluent;
+using Xunit;
+
+namespace NetElevation.Core.Test
+{
+    public class TileTreeNodeTest
+    {
+        [Fact]
+        public void TestGetTile()
+        {
+            var emptyNode = new TileTreeNode(10, 10, 10, 10);
+            Check.That(emptyNode.GetTile(10, 10)).IsNull();
+
+            var singleTileNode = new TileTreeNode(45, 45, 10, 10);
+            var lonelyTile = new TileInfo(45, 45, 10, 10, 1, 1);
+            singleTileNode.AddTiles(lonelyTile);
+            Check.That(singleTileNode.GetTile(-15, -15)).IsNull();
+            Check.That(singleTileNode.GetTile(45, 45)).IsEqualTo(lonelyTile);
+            Check.That(singleTileNode.GetTile(40, 50)).IsEqualTo(lonelyTile);
+
+            var twinTileNode = new TileTreeNode(30, 30, 5, 10);
+            var twinTile1 = new TileInfo(30, 30, 5, 5, 1, 1);
+            var twinTile2 = new TileInfo(30, 35, 5, 5, 1, 1);
+            twinTileNode.AddTiles(twinTile1, twinTile2);
+            Check.That(twinTileNode.GetTile(30, 30)).IsEqualTo(twinTile1);
+            Check.That(twinTileNode.GetTile(30, 35)).IsEqualTo(twinTile2);
+
+            var root = new TileTreeNode(90, -180, 180, 360);
+            root.AddChildren(singleTileNode, emptyNode, twinTileNode);
+
+            Check.That(root.GetTile(10, 10)).IsNull();
+            Check.That(root.GetTile(-15, -15)).IsNull();
+            Check.That(root.GetTile(45, 45)).IsEqualTo(lonelyTile);
+            Check.That(root.GetTile(40, 50)).IsEqualTo(lonelyTile);
+            Check.That(root.GetTile(30, 30)).IsEqualTo(twinTile1);
+            Check.That(root.GetTile(30, 35)).IsEqualTo(twinTile2);
+        }
+    }
+}
---
+++ NetElevation.Core/TileTreeNode.cs
@@ -0,0 +1,70 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetElevation.Core
+{
+    public class TileTreeNode
+    {
+        private readonly List<TileInfo> _tiles;
+        private List<TileTreeNode> _children;
+
+        public TileTreeNode(double north, double west, double latitudeSpan, double longitudeSpan)
+        {
+            North = north;
+            West = west;
+            LatitudeSpan = latitudeSpan;
+            LongitudeSpan = longitudeSpan;
+            _children = new List<TileTreeNode>();
+            _tiles = new List<TileInfo>();
+        }
+
+        public double North { get; }
+        public double West { get; }
+
+        public double LatitudeSpan { get; }
+        public double LongitudeSpan { get; }
+
+        public double South => North - LatitudeSpan;
+        public double East => West + LongitudeSpan;
+
+        public TileTreeNode? Parent { get; set; }
+        public IReadOnlyCollection<TileTreeNode> Children => _children.AsReadOnly();
+
+        private bool IsEmpty => !_children.Any() && !_tiles.Any();
+
+        public bool Contains(double latitude, double longitude)
+            => North >= latitude && latitude > South
+                && East > longitude && longitude >= West;
+
+        public void AddChildren(params TileTreeNode[] children)
+        {
+            _children.AddRange(children);
+            foreach (var child in children)
+            {
+                child.Parent = this;
+            }
+        }
+
+        public void AddTiles(params TileInfo[] tiles) => _tiles.AddRange(tiles);
+
+        public void RemoveEmptyChildren() => _children = _children.Where(c => !c.IsEmpty).ToList();
+
+        public TileInfo? GetTile(double latitude, double longitude)
+        {
+            if (IsEmpty || !Contains(latitude, longitude))
+            {
+                return null;
+            }
+            else if (_children.Any())
+            {
+                var child = _children.FirstOrDefault(c => c.Contains(latitude, longitude));
+                return child?.GetTile(latitude, longitude);
+            }
+            else
+            {
+                return _tiles.First(t => t.Contains(latitude, longitude));
+            }
+        }
+    }
+}
